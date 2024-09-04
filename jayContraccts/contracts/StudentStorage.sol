439bcefa057369cf03efca6f7421caa2ddfc4175
jay <zsjayy@163.com>
2024-09-04 23:56:26 +0800

    fix

---
+++ jayContraccts/contracts/StudentStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract StudentStorage{
+    // 这两个是状态变量，存储在storage，高gas消费
+    uint age;
+    string name;
+
+    function setData(string memory _name, uint _age) public{
+        //_name,_age是形参，中转传递值，使用memory存储，低gas消费
+        name = _name;
+        age = _age;
+        
+    }
+}
---
+++ jayContraccts/studyRecords/study02.md
@@ -0,0 +1,19 @@
+contract 后面的名字要和文件名称一致
+![alt text](image.png)
+
+function setData(){
+
+}
+
+funtion getData(){
+
+}
+
+函数被标记成view还是pure？
+view--试图函数，只访问，不修改状态
+pure--纯函数，不访问，不修改
+
+存储
+sotrage--
+memory--传递的变量中转
+calldata--
