76a53ef7b4b182ca90bf72da4f7f5e7c7b4c22da
jay <zsjayy@163.com>
2024-09-05 18:49:12 +0800

    add 2_deploy.js

--- jayContraccts/contracts/StudentListStorage.sol
+++ jayContraccts/contracts/StudentListStorage.sol
@@ -11,14 +11,14 @@ contract StudentListStorage{
     //动态数组
     Student[] public StudentList;
     //set变量
-    function addList(string memory _name,uint _age) public returns (unit){
+    function addList(string memory _name,uint _age) public returns (uint){
         uint count = StudentList.length;
         uint index = count+1;
         StudentList.push(Student(index,_name,_age));
         return StudentList.length;
     }
     //get变量
-    function getList() public view returns (Student[] memroy){
+    function getList() public view returns (Student[] memory){
         Student[] memory list = StudentList;
         return list;
     }
